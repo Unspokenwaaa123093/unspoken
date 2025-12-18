0155a9ae74aa43df4c0c467d13196ac47de3ea0b
Unspokenwaaa123093 <k82zvpwbyz@privaterelay.appleid.com>
2025-12-18 02:10:44 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://klwfchmvbrvnrviwfstn.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtsd2ZjaG12YnJ2bnJ2aXdmc3RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTMxMTksImV4cCI6MjA4MTU2OTExOX0.TIH7hzpG-eCZschmhmVZZSlK9LGFFM4hZRYgKUUQK7s',
+};
